f9b151bd300b346c2f1a381d7f5fd2ae56c54f2d
accrane <austincrane@gmail.com>
2021-04-20 09:29:31 -0400

    refactor

--- events-override.php
+++ events-override.php
@@ -24,3 +24,30 @@ function custom_tribe_bellaworks_style() {
 	wp_register_style( 'calendar-styles',  plugin_dir_url( __FILE__ ) . 'calendar-styles.css' );
     wp_enqueue_style( 'calendar-styles' );
 }
+/**
+ * Defines alternative titles for various event views.
+ *
+ * @param  string $title
+ * @return string
+ */
+function ac_dont_show_social_icons_on_calendar( $title ) {
+
+	if( is_post_type_archive('tribe_events') || is_singular('tribe_events') ) {
+		return true;
+	}
+   
+}
+add_filter( 'addtoany_sharing_disabled', 'ac_dont_show_social_icons_on_calendar' );
+function ac_lets_add_the_cal_title( $title ) {
+
+	if( is_post_type_archive('tribe_events') ) {
+		$title = 'Calendar';
+	}
+	// if(is_singular('tribe_events')) {
+	// 	$title = 'Today';
+	// }
+   
+
+    return $title;
+}
+add_filter( 'the_title', 'ac_lets_add_the_cal_title' );
